cc8f22b811e0c1ff888111487d8aa7d71e0669e8
Swapnil <Swapnil.kasabe@afourtech.com>
2024-06-30 23:14:57 +0530

    Add index.js and App.css files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+<<<<<<< Updated upstream
 import App from "./App";
 import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+=======
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+>>>>>>> Stashed changes
